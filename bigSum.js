6328938d2a7e11c4e97b182b320de72f072aee80
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-30 17:36:43 +0200

    add function

---
+++ bigSum.js
@@ -0,0 +1,9 @@
+function aVeryBigSum(ar) {
+  // Write your code here
+  // get all elements in a array
+  let sum = 0;
+  for (let i = 0; i < ar.length; i++){
+      sum = sum + ar[i]
+  }
+  return sum;
+}

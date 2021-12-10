75dc1d1abc203f107ec472bd4d508c45d00e6cbc
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2021-12-10 16:44:32 +0200

    Add code from mob programming

--- bigSum.js
+++
@@ -1,9 +0,0 @@
-function aVeryBigSum(ar) {
-  // Write your code here
-  // get all elements in a array
-  let sum = 0;
-  for (let i = 0; i < ar.length; i++){
-      sum = sum + ar[i]
-  }
-  return sum;
-}
